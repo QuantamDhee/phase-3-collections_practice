66515787bbb49f5f3d5432e3086b3d00e00e3995
Johndhee <Johndheeballesteros13@gmail.com>
2021-05-18 10:20:04 -0700

    commit tree 6296d357a19b82eac2a4671784c8da1d8e7726ac

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,44 @@
+def sort_array_asc(arr)
+    arr.sort
+end
 
+def sort_array_desc(arr)
+    arr.sort.reverse
+end
+
+def sort_array_char_count(arr)
+    arr.sort do |a, b|
+        a.size <=> b.size
+    end
+end
+
+def swap_elements(arr)
+    arr[1], arr[2] = arr[2], arr[1]
+    arr
+end
+
+def reverse_array(arr)
+    arr.reverse
+end
+
+def kesha_maker(arr)
+    arr.each {|i| i[2] = "$"}
+end
+
+def find_a(arr)
+    arr.select {|a| a[0] == "a"}
+end
+
+def sum_array(arr)
+    arr.inject {|n, i| n + i }
+end
+
+def add_s(arr)
+    arr.each_with_index.collect {|w, index| 
+    if index == 1 
+    w
+    else 
+    "#{w}s"
+    end 
+}
+end
